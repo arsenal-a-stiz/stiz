b300968bc9683a98203f60951466c2db1157d6f5
arsenal-a-stiz <aleksei.linok@gmail.com>
2026-06-15 12:25:31 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'warehouse-cache-v1';
+const STATIC_ASSETS = [
+  '.',
+  'index.html',
+  'manifest.json',
+  'icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
